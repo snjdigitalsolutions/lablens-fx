d79dab813903d5e9182e4ec46c01c632878b098d
Jamey <javarobots74@gmail.com>
2026-03-10 20:18:12 -0400

    Adding in lablens-fx

---
+++ src/main/java/com/snjdigitalsolutions/lablensfx/LablensFxApplication.java
@@ -0,0 +1,13 @@
+package com.snjdigitalsolutions.lablensfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LablensFxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LablensFxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/snjdigitalsolutions/lablensfx/LablensFxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snjdigitalsolutions.lablensfx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LablensFxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

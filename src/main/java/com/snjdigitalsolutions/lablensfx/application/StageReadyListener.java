86c310d38cd53e97dbfe0a7fa9534cb481c84a83
Jamey <javarobots74@gmail.com>
2026-03-13 23:11:00 -0400

    SpringInitializableNodes performInitialization called just before primary stage shown.

--- src/main/java/com/snjdigitalsolutions/lablensfx/application/StageReadyListener.java
+++ src/main/java/com/snjdigitalsolutions/lablensfx/application/StageReadyListener.java
@@ -2,9 +2,14 @@ package com.snjdigitalsolutions.lablensfx.application;
 
 import com.snjdigitalsolutions.lablensfx.nodes.HostPane;
 import com.snjdigitalsolutions.springbootutilityfx.application.AbstractStageReadyListener;
+import com.snjdigitalsolutions.springbootutilityfx.event.StageReadyEvent;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
@@ -26,6 +31,5 @@ public class StageReadyListener extends AbstractStageReadyListener {
     public void performIntialization() {
         LOGGER.debug("Initializing...");
         stageReadyController.addHostPane();
-        stageReadyController.performIntialization();
     }
 }
--- src/main/java/com/snjdigitalsolutions/lablensfx/nodes/HostPanel.java
+++ src/main/java/com/snjdigitalsolutions/lablensfx/nodes/HostPanel.java
@@ -1,13 +1,23 @@
 package com.snjdigitalsolutions.lablensfx.nodes;
 
+import com.snjdigitalsolutions.lablensfx.properties.StatusBarProperties;
+import com.snjdigitalsolutions.springbootutilityfx.event.StageReadyEvent;
 import com.snjdigitalsolutions.springbootutilityfx.node.SpringInitializableNode;
 import com.snjdigitalsolutions.springbootutilityfx.node.utility.NodeLoader;
+import jakarta.annotation.PostConstruct;
+import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +25,8 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("prototype")
 public class HostPanel extends GridPane implements SpringInitializableNode {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HostPanel.class);
 
     @FXML
     private Label hostNameLabel;
@@ -25,12 +37,40 @@ public class HostPanel extends GridPane implements SpringInitializableNode {
     @FXML
     private Label descriptionLabel;
 
-    public HostPanel(@Value("classpath:/fxml/HostPanel.fxml") Resource fxml){
+    private final StatusBarProperties statusBarProperties;
+
+    public HostPanel(@Value("classpath:/fxml/HostPanel.fxml") Resource fxml, StatusBarProperties statusBarProperties){
+        this.statusBarProperties = statusBarProperties;
         NodeLoader.load(fxml, this);
     }
 
+    @PostConstruct
     @Override
     public void performIntialization() {
+        addSelectedStyle(this);
+        this.getChildren().forEach(node -> {
+            if (node instanceof Label){
+                addSelectedStyle(node);
+            }
+        });
+    }
+
+    private void addSelectedStyle(Node node) {
+        node.setOnMouseClicked(event -> {
+            if (node.getStyleClass().contains("host-panel-selected")) {
+                node.getStyleClass().remove("host-panel-selected");
+                int currentValue = statusBarProperties.numberOfSelectedHostsProperty().getValue();
+                currentValue--;
+                LOGGER.debug("Panel deselected - {}", currentValue);
+                statusBarProperties.numberOfSelectedHostsProperty().set(currentValue);
+            } else {
+                node.getStyleClass().add("host-panel-selected");
+                int currentValue = statusBarProperties.numberOfSelectedHostsProperty().getValue();
+                currentValue++;
+                LOGGER.debug("Panel selected - {}", currentValue);
+                statusBarProperties.numberOfSelectedHostsProperty().set(currentValue);
+            }
+        });
 
     }
 
@@ -49,4 +89,6 @@ public class HostPanel extends GridPane implements SpringInitializableNode {
     public Label getDescriptionLabel() {
         return descriptionLabel;
     }
+
+
 }

016d92be0fe0fc6305c70845f64d5b573968dad7
Jamey <javarobots74@gmail.com>
2026-04-03 22:51:22 -0400

    Initial path addition

--- src/main/java/com/snjdigitalsolutions/lablensfx/nodes/ConfigurationPane.java
+++ src/main/java/com/snjdigitalsolutions/lablensfx/nodes/ConfigurationPane.java
@@ -1,11 +1,13 @@
 package com.snjdigitalsolutions.lablensfx.nodes;
 
+import com.snjdigitalsolutions.lablensfx.orm.ConfigurationPath;
+import com.snjdigitalsolutions.lablensfx.utility.FilePathValidator;
 import com.snjdigitalsolutions.springbootutilityfx.node.SpringInitializableNode;
+import com.snjdigitalsolutions.springbootutilityfx.node.utility.AlertUtility;
 import com.snjdigitalsolutions.springbootutilityfx.node.utility.NodeLoader;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.SplitPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -20,10 +22,16 @@ public class ConfigurationPane extends AnchorPane implements SpringInitializable
     private TextField filePathTextField;
     @FXML
     private Button addButton;
+    @FXML
+    private TableView<ConfigurationPath> selectedPathsTable;
 
     private final double splitPaneDividerPosition = 0.5;
+    private final FilePathValidator filePathValidator;
+    private final AlertUtility alertUtility;
 
-    public ConfigurationPane(@Value("classpath:/fxml/ConfigurationPane.fxml") Resource fxml) {
+    public ConfigurationPane(@Value("classpath:/fxml/ConfigurationPane.fxml") Resource fxml, FilePathValidator filePathValidator, AlertUtility alertUtility) {
+        this.filePathValidator = filePathValidator;
+        this.alertUtility = alertUtility;
         NodeLoader.load(fxml, this);
     }
 
@@ -38,10 +46,22 @@ public class ConfigurationPane extends AnchorPane implements SpringInitializable
                     }
                 });
         addButton.setOnAction(event -> {
+            if (filePathValidator.isValid(filePathTextField.getText())) {
+                ConfigurationPath newPath = new ConfigurationPath();
+                newPath.setConfigurationPath(filePathTextField.getText());
+                selectedPathsTable.getItems().add(newPath);
+            } else {
+                alertUtility.warningAlert("Invalid Path", "The path entered is not a valid system path.");
+            }
 
+            TableColumn<ConfigurationPath, String> pathColumn = new TableColumn<>("Path");
+            pathColumn.setCellValueFactory(new PropertyValueFactory<>("path"));
+            selectedPathsTable.getColumns().add(pathColumn);
         });
     }
 
-    private void validateFilePath() {}
+    private void validateFilePath() {
+
+    }
 
 }
--- src/main/java/com/snjdigitalsolutions/lablensfx/orm/ConfigurationPath.java
+++ src/main/java/com/snjdigitalsolutions/lablensfx/orm/ConfigurationPath.java
@@ -1,13 +1,13 @@
 package com.snjdigitalsolutions.lablensfx.orm;
 
 import jakarta.persistence.*;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Table(name = "configuration_path")
-@Getter
-@Setter
 public class ConfigurationPath {
 
     @Id
@@ -21,4 +21,54 @@ public class ConfigurationPath {
     @Column(name = "requires_elevation")
     private Boolean requiresElevation;
 
+    @Transient
+    private StringProperty path = new SimpleStringProperty();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ComputeResource getComputeResource() {
+        return computeResource;
+    }
+
+    public void setComputeResource(ComputeResource computeResource) {
+        this.computeResource = computeResource;
+    }
+
+    public String getConfigurationPath() {
+        return configurationPath;
+    }
+
+    public void setConfigurationPath(String configurationPath) {
+        this.configurationPath = configurationPath;
+        this.pathProperty().setValue(configurationPath);
+    }
+
+    public Boolean getRequiresElevation() {
+        return requiresElevation;
+    }
+
+    public void setRequiresElevation(Boolean requiresElevation) {
+        this.requiresElevation = requiresElevation;
+    }
+
+    @Transient
+    public String getPath() {
+        return path.get();
+    }
+
+    @Transient
+    public StringProperty pathProperty() {
+        return path;
+    }
+
+    @Transient
+    public void setPath(String path) {
+        this.path.set(path);
+    }
 }

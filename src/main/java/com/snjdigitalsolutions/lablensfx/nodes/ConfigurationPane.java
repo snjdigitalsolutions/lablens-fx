bb0aeadb01cb5457a9c01835cfee65a6fe33fc36
Jamey <jparham@snjdigitalsolutions.com>
2026-04-04 10:45:46 -0400

    Experiments with table cell factory

--- src/main/java/com/snjdigitalsolutions/lablensfx/nodes/ConfigurationPane.java
+++ src/main/java/com/snjdigitalsolutions/lablensfx/nodes/ConfigurationPane.java
@@ -5,10 +5,13 @@ import com.snjdigitalsolutions.lablensfx.utility.FilePathValidator;
 import com.snjdigitalsolutions.springbootutilityfx.node.SpringInitializableNode;
 import com.snjdigitalsolutions.springbootutilityfx.node.utility.AlertUtility;
 import com.snjdigitalsolutions.springbootutilityfx.node.utility.NodeLoader;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Callback;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
@@ -53,14 +56,36 @@ public class ConfigurationPane extends AnchorPane implements SpringInitializable
             } else {
                 alertUtility.warningAlert("Invalid Path", "The path entered is not a valid system path.");
             }
-
-            TableColumn<ConfigurationPath, String> pathColumn = new TableColumn<>("Path");
-            pathColumn.setCellValueFactory(new PropertyValueFactory<>("path"));
-            selectedPathsTable.getColumns().add(pathColumn);
         });
-    }
 
-    private void validateFilePath() {
+
+        TableColumn<ConfigurationPath, String> pathColumn = new TableColumn<>("Path");
+        pathColumn.setCellValueFactory(path -> path.getValue().pathProperty());
+
+        pathColumn.setCellFactory(col -> new TableCell<ConfigurationPath, String>() {
+            private final Label pathLabel = new Label();
+
+//                {
+//                    // Constructor block — runs once per cell instance
+//                    pathLabel.getStyleClass().add("status-badge");
+//                }
+
+            @Override
+            protected void updateItem(String status, boolean empty) {
+                super.updateItem(status, empty);
+                if (empty || status == null) {
+                    setGraphic(null);
+                } else {
+                    pathLabel.setText(status);
+//                        pathLabel.getStyleClass().removeAll("status-online", "status-offline");
+//                        pathLabel.getStyleClass().add("status-" + status.toLowerCase());
+                    setGraphic(pathLabel);
+                }
+            }
+        });
+        pathColumn.prefWidthProperty().bind(selectedPathsTable.widthProperty().subtract(2));
+        selectedPathsTable.getColumns().add(pathColumn);
+        selectedPathsTable.setItems(FXCollections.observableArrayList());
 
     }
 

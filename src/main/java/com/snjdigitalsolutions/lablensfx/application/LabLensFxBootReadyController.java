c75b81d63a5d865029d15288d742f01985eb7fe1
Jamey <javarobots74@gmail.com>
2026-06-14 11:56:07 -0400

    Removed sample code fro graphing

--- src/main/java/com/snjdigitalsolutions/lablensfx/application/LabLensFxBootReadyController.java
+++ src/main/java/com/snjdigitalsolutions/lablensfx/application/LabLensFxBootReadyController.java
@@ -244,53 +244,6 @@ public class LabLensFxBootReadyController implements SpringInitializableNode {
 
         testFunctionMenuItem.setOnAction(event -> {
 //           Thread.ofVirtual().start(configurationChangeCheckTaskObjectProvider.getObject());
-
-            Graph<String, String> g = new GraphEdgeList<>();
-
-            g.insertVertex("A");
-            g.insertVertex("B");
-            g.insertVertex("C");
-            g.insertVertex("D");
-            g.insertVertex("E");
-            g.insertVertex("F");
-            g.insertVertex("G");
-
-            g.insertEdge("A", "B", "1");
-            g.insertEdge("A", "C", "2");
-            g.insertEdge("A", "D", "3");
-            g.insertEdge("A", "E", "4");
-            g.insertEdge("A", "F", "5");
-            g.insertEdge("A", "G", "6");
-
-            g.insertVertex("H");
-            g.insertVertex("I");
-            g.insertVertex("J");
-            g.insertVertex("K");
-            g.insertVertex("L");
-            g.insertVertex("M");
-            g.insertVertex("N");
-
-            g.insertEdge("H", "I", "7");
-            g.insertEdge("H", "J", "8");
-            g.insertEdge("H", "K", "9");
-            g.insertEdge("H", "L", "10");
-            g.insertEdge("H", "M", "11");
-            g.insertEdge("H", "N", "12");
-
-            g.insertEdge("A", "H", "0");
-
-            SmartPlacementStrategy initialPlacement = new SmartCircularSortedPlacementStrategy();
-            SmartGraphPanel<String, String> graphView = new SmartGraphPanel<>(g, initialPlacement);
-            Scene scene = new Scene(graphView, 1024, 768);
-
-            Stage stage = new Stage(StageStyle.DECORATED);
-            stage.setTitle("JavaFXGraph Visualization");
-            stage.setScene(scene);
-            stage.show();
-
-//IMPORTANT! - Called after scene is displayed, so we can initialize the graph visualization
-            graphView.init();
-
         });
         settingsButton.setOnAction(event -> {
             StageNodeBuilder.builder()
